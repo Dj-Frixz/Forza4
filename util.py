ead819b7a55428aa6eb13740beb214528aa99232
Dj-Frixz <67787064+Dj-Frixz@users.noreply.github.com>
2021-11-05 19:47:35 +0100

    better issymmetrical
    transfer from pcs

--- util.py
+++ util.py
@@ -1,10 +1,12 @@
 import numpy
 
 def issymmetrical(matrix):
-    a = numpy.rot90(matrix)
-    if numpy.equal(a[:3], numpy.flip(a[4:],1)):
+    rotmatrix = numpy.rot90(matrix)
+    sidea = rotmatrix[:3]
+    sidebflipped = numpy.flip(rotmatrix[4:],1)
+    if numpy.equal(sidea, sidebflipped):
         return 1    #it's symmetrical
-    elif :
+    elif sidea+sidebflipped:
         return 0    #could be symmetrical in the future
     else:
         return -1   #not symmetrical
