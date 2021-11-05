2291951b5ea01856e987de4c41f35682b3f2dcfb
Dj-Frixz <gmsccntdenmark@gmail.com>
2021-11-05 15:44:35 +0100

    symmetry check skeleton

--- util.py
+++ util.py
@@ -1,3 +1,10 @@
-def issimmetrical(matrix):
-    for i in range(3):  #better numpy function?
-        j = 0
+import numpy
+
+def issymmetrical(matrix):
+    a = numpy.rot90(matrix)
+    if numpy.equal(a[:3], numpy.flip(a[4:],1)):
+        return 1    #it's symmetrical
+    elif :
+        return 0    #could be symmetrical in the future
+    else:
+        return -1   #not symmetrical
