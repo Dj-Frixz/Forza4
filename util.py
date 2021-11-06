8733e0980b044c325bb59eb5a8045715891267e4
Dj-Frixz <gmsccntdenmark@gmail.com>
2021-11-06 18:34:33 +0100

    issymmetrical final optimization (?)

--- util.py
+++ util.py
@@ -1,12 +1,11 @@
-import numpy
+import numpy as np
 
 def issymmetrical(matrix):
-    rotmatrix = numpy.rot90(matrix) #rotates the matrix so slicing is possible among rows
-    sidea = rotmatrix[:3]
-    sideb_flipped = numpy.flip(rotmatrix[4:],0) #flips on the vertical axis (up and down) to match sidea
+    sidea, x, sideb = np.hsplit(matrix,np.array([3,4])) #rotates the matrix so slicing is possible among rows
+    sideb_flipped = np.fliplr(sideb) #flips on the horizontal axis (left and right) to match sidea
     sides_sum = sidea + sideb_flipped
 
-    if numpy.amin(numpy.equal(sidea, sideb_flipped)):
+    if np.amin(np.equal(sidea, sideb_flipped)):
         return 1    #it's symmetrical
     elif sides_sum[sides_sum==3].size==0:   #search for 3s, which can appear only if 2+1, in which case the board can't return symmetrical anymore
         return 0    #could be symmetrical in the future
