b9ec87c60c763c8ee4ea5bb774d20c7214fa992b
Dj-Frixz <gmsccntdenmark@gmail.com>
2021-11-09 15:22:40 +0100

    BIG UPDATE (same as before, dunno why again)

--- util.py
+++ util.py
@@ -1,13 +1,17 @@
 import numpy as np
 
 def issymmetrical(matrix):
-    sidea, x, sideb = np.hsplit(matrix,np.array([3,4])) #rotates the matrix so slicing is possible among rows
-    sideb_flipped = np.fliplr(sideb) #flips on the horizontal axis (left and right) to match sidea
-    sides_sum = sidea + sideb_flipped
+    #slice the matrix vertically
+    sidea, x, sideb = np.hsplit(matrix,np.array([3,4]))
+    #flip 'left and right' sideb to match sidea
+    sideb_flipped = np.fliplr(sideb)
 
-    if np.amin(np.equal(sidea, sideb_flipped)):
+    if np.array_equal(sidea, sideb_flipped):
         return 1    #it's symmetrical
-    elif sides_sum[sides_sum==3].size==0:   #search for 3s, which can appear only if 2+1, in which case the board can't return symmetrical anymore
-        return 0    #could be symmetrical in the future
     else:
-        return -1   #not symmetrical
+        sides_sum = sidea + sideb_flipped
+        #search for 3s, which can only appear if '2+1', in which case the board can't return symmetrical anymore
+        if np.array_equal(sides_sum[sides_sum==3],[]):
+            return 0    #could be symmetrical in the future
+        else:
+            return -1   #not symmetrical
